841ebf23e6e035266acbbcb1b8eecd7f1fae0eae
Julien Buonarota <j.buohk@gmail.com>
2022-05-25 15:59:11 +0200

    Recuperation of ranks informations and test of BGG API V1
    The bgg api v1 does not offer extra statistics

--- script_bgg_api.py
+++ script_bgg_api.py
@@ -2,6 +2,7 @@
 import requests
 import xml.etree.ElementTree as ET
 
+################ API V2 ################
 ##
 base_url = "https://boardgamegeek.com/xmlapi2/"
 hot_request = "hot?type=boardgame"
@@ -17,20 +18,44 @@ for child in root:
 thing_request = "thing?"
 
 r_id = requests.get(base_url + thing_request + "id=342942&stats=1")
-root_id = ET.fromstring(r.content)
+root_id = ET.fromstring(r_id.content)
 
 ## primary name (others are translations)
-name_raw = root.find(".//name[@type='primary']")
+name_raw = root_id.find(".//name[@type='primary']")
 primary_name = name_raw.attrib['value']
 
 ## statistics
-stats = root[0].find('statistics')[0]
+stats = root_id[0].find('statistics')[0]
 stats_d = {}
+
+for i in stats.find('ranks'):
+    print(i.attrib['friendlyname'], i.attrib['value'])
 
 for child in stats:
     try:
-        stats_d[child.tag] = eval(child.attrib['value'])
+        # handling of ranks nesting and unpredictable length
+        if child.tag == 'ranks':
+            d = {}
+            for i in child:
+                d[i.attrib['friendlyname']] = eval(i.attrib['value'])
+            stats_d['ranks'] = d
+        else:
+            stats_d[child.tag] = eval(child.attrib['value'])
     except KeyError:
         stats_d[child.tag] = None
 
+################ API V1 ################
+## no differences in available statistics
+base_url_v1 = "https://boardgamegeek.com/xmlapi/"
 
+## boardgame
+r_id = requests.get(base_url_v1 + "boardgame/" + "342942?stats=1")
+root_id = ET.fromstring(r_id.content)
+
+for child in root_id[0]:
+    print(child.tag)
+
+a = root_id[0].findall('poll')
+for i in a:
+    for child in i:
+        print(child.tag, child.attrib)
