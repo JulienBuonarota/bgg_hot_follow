45458dafb5d3141ce1b05430a6bd2f07ff1ac758
Julien Buonarota <j.buohk@gmail.com>
2022-07-11 16:09:52 +0200

    Visualization of hotness rank for a given board game.
    Used epoch to extrapolate a time serie and keep the proper time
    difference on the x absis.

--- script_data_visualisation.py
+++ script_data_visualisation.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import psycopg2
 import matplotlib.pyplot as plt
+import matplotlib.dates as dates
+import scipy.interpolate as sci
+import numpy as np
 
 ## Get data of specific game
 bgg_game_id = 101
@@ -16,9 +19,39 @@ with psycopg2.connect("dbname=BGGDB user=bgg password=admin host=localhost") as 
 
 data = pd.DataFrame(data, columns=['id', 'timestamp', 'hotness_list'])
 
+
 ## compute game position in hotness list
 data['rank'] = data['hotness_list'].apply(lambda x: x.index(bgg_game_id))
 
-## plot
-data.plot('timestamp', 'rank')
+## PLOT
+x = data[['timestamp']].to_numpy().squeeze()
+y = data[['rank']].to_numpy().squeeze() + 1
 
+# CURVE INTERPOLATION using epoch
+x_epoch = np.array([int(i - np.datetime64("1970-01-01")) for i in x])
+cubic_interploation_model = sci.interp1d(x_epoch, y, kind = "cubic")
+
+X_ = np.linspace(int(x_epoch.min()), int(x_epoch.max()), 500)
+Y_ = cubic_interploation_model(X_)
+# for ploting using matplotlib date tools
+X_timestamp = np.array(X_, dtype='datetime64[ns]')
+
+## plot
+fig, ax = plt.subplots()
+ax.plot(X_timestamp, Y_)
+#ax.plot(x, y)
+
+ax.invert_yaxis()
+
+# ax.set_ylim(top=0)
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('title')
+
+ax.xaxis.set_major_locator(dates.DayLocator(interval=3))    # every 3 days
+ax.xaxis.set_major_formatter(dates.DateFormatter('\n%d-%m-%Y')) 
+plt.show()
+
+
+    
