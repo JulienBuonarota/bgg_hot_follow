f1891be39f0eabea4c1075f8fc268d24637ab451
Julien Buonarota <j.buohk@gmail.com>
2022-07-11 16:08:53 +0200

    Added tools to save list to DB

--- tool_DB.py
+++ tool_DB.py
@@ -1,6 +1,20 @@
 import psycopg2
 import datetime
 
+def save_list_to_DB(l, table_name):
+    """ TODO """
+    with psycopg2.connect("dbname=BGGDB user=bgg password=admin host=localhost") as conn:
+        for i in l:
+            with conn.cursor() as cur:
+                try:
+                    cur.execute(
+                        """
+                        INSERT INTO boardgame_review(review_time, primary_name, bgg_id, review_type, notes)
+                        VALUES (%s, %s, %s, %s, %s);""",
+                        i)
+                except psycopg2.errors.UniqueViolation:
+                    print('Unique constraint violated')
+
 def save_boardgame_info(bgg_id, primary_name, designer_list, year_published):
     # check if boardgame already in DB
     with psycopg2.connect("dbname=BGGDB user=bgg password=admin host=localhost") as conn:
